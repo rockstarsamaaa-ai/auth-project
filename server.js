a6b3e5cc70b0017198212e66f109d63cb230f145
rockstarsamaaa-ai <rockstarsamaaa@gmail.com>
2026-04-17 11:39:35 +0530

    root routes n

--- server.js
+++ server.js
@@ -1,25 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
-const cors = require("cors");
 
 const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
 
 // Routes
 app.use("/api/auth", require("./routes/authRoutes"));
 
-//  Root route (prevents "Cannot GET /")
-app.get("/", (req, res) => {
-  res.send("Auth API Running...");
-});
-
 const PORT = process.env.PORT || 5000;
 
-//  Connect DB FIRST, then start server
+//  Connect DB and THEN start server
 connectDB()
   .then(() => {
     app.listen(PORT, () => {
