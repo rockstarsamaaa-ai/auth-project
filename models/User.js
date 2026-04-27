b6fcb0ce3117347ef06cafa201ecd52dc6816895
rockstarsamaaa-ai <rockstarsamaaa@gmail.com>
2026-04-27 15:54:40 +0530

    Refactor password hashing middleware to remove unnecessary next() call

--- models/User.js
+++ models/User.js
@@ -22,13 +22,12 @@ const userSchema = new mongoose.Schema({
   }
 }, { timestamps: true });
 
-userSchema.pre("save", async function savePassword(next) {
+userSchema.pre("save", async function savePassword() {
   if (!this.isModified("password")) {
-    return next();
+    return;
   }
 
   this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
 
 userSchema.methods.comparePassword = function comparePassword(password) {
